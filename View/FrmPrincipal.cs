5a6c0a42b5b809193cf502f3710f38ecc6d89292
Thiago Christopher <thiago_cfc@hotmail.com>
2019-04-24 18:47:34 -0300

    Adicionando a rotina para abrir as telas

--- View/FrmPrincipal.cs
+++ View/FrmPrincipal.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace chat_aps.View
@@ -15,6 +8,18 @@ namespace chat_aps.View
         public FrmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void BtnEntrar_Click(object sender, EventArgs e)
+        {
+            new FrmChat().Show();
+            Hide();
+        }
+
+        private void BtnCriar_Click(object sender, EventArgs e)
+        {
+            new FrmSala().Show();
+            Hide();
         }
     }
 }
