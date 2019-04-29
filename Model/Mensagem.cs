1e296c3b42e0ef0810ad4c590b89bfdcdfb76488
Thiago Christopher <thiago_cfc@hotmail.com>
2019-04-28 22:26:47 -0300

    Adicionando a classe padrão das mensagens de erro

---
+++ Model/Mensagem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chat_aps.Model
+{
+    static class Mensagem
+    {
+
+        private static void Message(string message)
+        {
+            MessageBox.Show(message, "C\\ Chat", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void MessageError(string message)
+        {
+            Message(message);
+        }
+
+        public static void MessageInfo(string message)
+        {
+            Message(message);
+        }
+
+        
+
+    }
+}
