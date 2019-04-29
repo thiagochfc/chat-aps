9d155f2d5e7117a6c1c789222a7eef87be3d421d
Thiago Christopher <thiago_cfc@hotmail.com>
2019-04-28 22:30:27 -0300

    Ajustes na forma de fechar o chat e alguns ajustes na exceção

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -73,34 +74,41 @@ namespace chat_aps.Model
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Erro : " + ex.Message, "Erro na conexão com servidor", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Mensagem.MessageError(ex.Message);
             }
         }
 
         private void RecebeMensagens()
         {
-            // Recebe a resposta do servidor
-            StrReceptor = new StreamReader(TcpServidor.GetStream());
-            string ConexaoResposta = StrReceptor.ReadLine();
-
-            // Se o primeiro caracater da resposta é 1 a conexão foi feita com sucesso
-            if (ConexaoResposta[0] == '1')
-                // Atualiza o formulário para informar que esta conectado
-                View.Invoke(new AtualizaLogCallBack(AtualizaLog), new object[] { "Conectado com sucesso!" });
-            else // Se o primeiro caractere não for 1 a conexão falhou
+            try
             {
-                string Motivo = "Não Conectado: ";
-                // Extrai o motivo da mensagem resposta. O motivo começa no 3º caractere
-                Motivo += ConexaoResposta.Substring(2, ConexaoResposta.Length - 2);
-                // Atualiza o formulário como o motivo da falha na conexão
-                View.Invoke(new FechaConexaoCallBack(FechaConexao), new object[] { Motivo });
-                // Sai do método
-                return;
+                // Recebe a resposta do servidor
+                StrReceptor = new StreamReader(TcpServidor.GetStream());
+                string ConexaoResposta = StrReceptor.ReadLine();
+
+                // Se o primeiro caracater da resposta é 1 a conexão foi feita com sucesso
+                if (ConexaoResposta[0] == '1')
+                    // Atualiza o formulário para informar que esta conectado
+                    View.Invoke(new AtualizaLogCallBack(AtualizaLog), new object[] { "Conectado com sucesso!" });
+                else // Se o primeiro caractere não for 1 a conexão falhou
+                {
+                    string Motivo = "Não Conectado: ";
+                    // Extrai o motivo da mensagem resposta. O motivo começa no 3º caractere
+                    Motivo += ConexaoResposta.Substring(2, ConexaoResposta.Length - 2);
+                    // Atualiza o formulário como o motivo da falha na conexão
+                    View.Invoke(new FechaConexaoCallBack(FechaConexao), new object[] { Motivo });
+                    // Sai do método
+                    return;
+                }
+                // Enquanto estiver conectado le as linhas que estão chegando do servidor
+                while (Conectado)
+                    // Exibe mensagems no Textbox
+                    View.Invoke(new AtualizaLogCallBack(AtualizaLog), new object[] { StrReceptor.ReadLine() });
             }
-            // Enquanto estiver conectado le as linhas que estão chegando do servidor
-            while (Conectado)
-                // Exibe mensagems no Textbox
-                View.Invoke(new AtualizaLogCallBack(AtualizaLog), new object[] { StrReceptor.ReadLine() });
+            catch (IOException IOE)
+            {
+                Mensagem.MessageError(IOE.Message);
+            }
         }
 
         private void AtualizaLog(string strMensagem)
@@ -113,36 +121,35 @@ namespace chat_aps.Model
         {
             if (TxtMensagem.Lines.Length >= 1)
             {
-                StwEnviador.WriteLine(TxtMensagem.Text);
+                StwEnviador.WriteLine(TxtMensagem.Text.Trim());
                 StwEnviador.Flush();
                 TxtMensagem.Lines = null;
             }
             TxtMensagem.Text = "";
         }
 
-        internal void FechaConexao(string Motivo)
+        private void FechaObjetos()
         {
-            // Mostra o motivo porque a conexão encerrou
-            TxtLog.AppendText(Motivo + "\r\n");
-            
-            // Fecha os objetos
             Conectado = false;
             StwEnviador.Close();
             StrReceptor.Close();
             TcpServidor.Close();
         }
 
+        internal void FechaConexao(string Motivo)
+        {
+            // Mostra o motivo porque a conexão encerrou
+            TxtLog.AppendText(Motivo + "\r\n");
+
+            // Fecha os objetos
+            FechaObjetos();
+        }
+
         // O tratador de evento para a saida da aplicação
         public void OnApplicationExit(object sender, EventArgs e)
         {
-            if (Conectado == true)
-            {
-                // Fecha as conexões, streams, etc...
-                Conectado = false;
-                StwEnviador.Close();
-                StrReceptor.Close();
-                TcpServidor.Close();
-            }
+            // Fecha as conexões, streams, etc...
+            FechaObjetos();
         }
     }
 }
