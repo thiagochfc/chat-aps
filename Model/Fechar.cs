e0ce4a8fe572d742cbc7231a5aefa7f31c1720e5
Thiago Christopher <thiago_cfc@hotmail.com>
2019-04-28 22:27:08 -0300

    Adicionando a classe padrão para fechar os Forms

---
+++ Model/Fechar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace chat_aps.Model
+{
+    static class Fechar
+    {
+        public static void OnApplicationExit(object sender, EventArgs e)
+        {
+            var process = Process.GetProcessesByName("chat-aps.exe");
+            foreach (var p in process)
+            {
+                p.Kill();
+                p.WaitForExit();
+            }
+        }
+    }
+}
